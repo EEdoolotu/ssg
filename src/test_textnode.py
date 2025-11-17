084e7954b70221ad07cb03929fa1577508f45d23
EEdoolotu <ejirogheneedoolotu@gmail.com>
2025-11-17 16:56:39 +0000

    ssg

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 from htmlnode import LeafNode
 from textnode import TextNode, TextType, text_node_to_html_node
+from markdown_converter import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +24,76 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("This is **bold** text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0], TextNode("This is ", TextType.TEXT))
+        self.assertEqual(new_nodes[1], TextNode("bold", TextType.BOLD))
+        self.assertEqual(new_nodes[2], TextNode(" text", TextType.TEXT))
+
+    def test_split_nodes_delimiter_code(self):
+        node = TextNode("This is `code` here", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0], TextNode("This is ", TextType.TEXT))
+        self.assertEqual(new_nodes[1], TextNode("code", TextType.CODE))
+        self.assertEqual(new_nodes[2], TextNode(" here", TextType.TEXT))
+
+    def test_split_nodes_delimiter_multiple(self):
+        node = TextNode("**bold** and **more bold**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0], TextNode("bold", TextType.BOLD))
+        self.assertEqual(new_nodes[1], TextNode(" and ", TextType.TEXT))
+        self.assertEqual(new_nodes[2], TextNode("more bold", TextType.BOLD))
+
+    def test_split_nodes_delimiter_non_text_node(self):
+        node = TextNode("Already bold", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(new_nodes), 1)
+        self.assertEqual(new_nodes[0], node)
+
+    def test_split_nodes_delimiter_invalid(self):
+        node = TextNode("Unmatched **delimiter", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_multiple(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual([
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+        ], matches)
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev")
+        ], matches)
+
+    def test_extract_markdown_links_single(self):
+        matches = extract_markdown_links("Check out [this link](https://example.com)")
+        self.assertListEqual([("this link", "https://example.com")], matches)
+
+    def test_extract_no_images(self):
+        matches = extract_markdown_images("Just plain text here")
+        self.assertListEqual([], matches)
+
+    def test_extract_no_links(self):
+        matches = extract_markdown_links("Just plain text here")
+        self.assertListEqual([], matches)
 
 
 if __name__ == "__main__":
